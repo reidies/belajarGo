70b2cd937370be7db4191920b02aafecec9270ff
reidies <silence_corp2001@yahoo.com>
2021-01-15 01:22:39 +0700

    tambahan

--- main.go
+++ main.go
@@ -1,6 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+type abstracts struct {
+	T1 string
+	T2 int
+	T3 bool
+	T4 float64
+}
 
 func main() {
 
@@ -10,6 +20,47 @@ func main() {
 	conditional()
 	arrays()
 	mapping()
+	switchingexampe()
+	iteration()
+
+	value1:= 20
+	value2:=21
+
+	fmt.Println("calculating", calculation(value1,value2))
+	bigger,smaler := isItbigger(value1,value2)
+	fmt.Println("is it bigger", bigger, "istItsmaller", smaler)
+	multiple, division := multipleDivision(float64(value1), float64(value2))
+	fmt.Println("multiple", multiple, "division", division)
+
+	numbers := []int {10,20,30,40,50}
+	fmt.Println("the total of", sumAll(numbers...))
+}
+
+func calculation(value1 int, value2 int)int{
+	//with specific return value
+	return value1+value2
+}
+
+func isItbigger(value1 int, value2 int) (bool, bool){
+	bigger := value1>value2
+	smaller := value1<value2
+	return bigger, smaller
+}
+
+func multipleDivision (value1 float64, value2 float64)(multiplication float64, division float64){
+	multiplication = value1 * value2
+	division = value2/value1
+
+	return
+}
+
+func sumAll(numbers ...int) int{
+	total :=0
+	for _, number := range numbers {
+		total +=number
+	}
+
+	return total
 }
 
 
@@ -18,7 +69,7 @@ func variables(){
 		firstName = "heman"
 		lastName = "setia"
 	)
-
+	sayHelloTo(firstName, lastName)
 	age := 32
 
 	const job = "programmer"
@@ -44,8 +95,11 @@ func conditional(){
 	fmt.Println(name1 == name2, name1<name2)
 }
 
-func arrays(){
+func sayHelloTo(firstName string, lastName string){
+	fmt.Println("hello", firstName, lastName)
+}
 
+func arrays(){
 
 	var names = [3] string {"adi", "rudi"}
 	fmt.Println(names, len(names))
@@ -74,7 +128,49 @@ func mapping(){
 	var books = make(map[string] string)
 	books ["title"] = "golang"
 	books ["author"] = "rudi"
-	books ["harga"] = string(123)
+	books ["harga"] = strconv.Itoa(123)
 	fmt.Println(books)
-	fmt.Println(books["title"], books["title"] ,books["harga"])
+	fmt.Println(books["title"], books["author"] ,books["harga"])
 }
+
+func switchingexampe(){
+	name := "budis"
+
+	switch  lenght := len(name); lenght<5 {
+	case true :
+		fmt.Println("name pendek")
+	case false :
+		fmt.Println("name panjang")
+	}
+
+	length := len(name)
+	switch  {
+	case length<5:
+		fmt.Println("nama pendek")
+	case length >5:
+		fmt.Println("name panjang")
+	default:
+		fmt.Println("hello", name)
+
+	}
+}
+
+func iteration(){
+	for counter:=1; counter<=100;counter++{
+
+		if counter%2 ==0{
+			continue
+		}
+		fmt.Print( counter)
+
+		if counter >10 {break}
+	}
+
+	names := []string{"raden", "sakit", "jiwa"}
+
+	for index, name:= range names{
+		fmt.Print("index", index, "=", name)
+	}
+
+}
+
